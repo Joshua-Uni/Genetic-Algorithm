7d7230f9ebac29ef349fd7f27aec72d3438e3247
Joshua <112139913+Joshua-Uni@users.noreply.github.com>
2023-07-11 14:48:04 +1000

    Add files via upload

---
+++ comprun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# exit script at first error
+set -u -e
+
+# Compile it and output to executable called 'app'
+clang Main.c -I/Library/Frameworks/SDL2.framework/Headers -F/Library/Frameworks -framework SDL2 -o run
+
+# runs the app
+./run
